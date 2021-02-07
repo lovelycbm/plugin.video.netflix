59a3027c029be74379551fd1534d72ba463ad414
CastagnaIT <gottardo.stefano.83@gmail.com>
2021-02-07 10:40:20 +0100

    Enabled TCP Keep-Alive to Requests

--- resources/lib/services/msl/msl_requests.py
+++ resources/lib/services/msl/msl_requests.py
@@ -10,10 +10,14 @@
 """
 import base64
 import json
+import socket
+import sys
 import time
 import zlib
 
 import requests.exceptions as req_exceptions
+from requests.adapters import HTTPAdapter, DEFAULT_POOLBLOCK
+from urllib3 import PoolManager, HTTPConnectionPool, HTTPSConnectionPool
 
 import resources.lib.common as common
 from resources.lib.common.exceptions import MSLError
@@ -25,6 +29,76 @@ from resources.lib.utils.esn import get_esn
 from resources.lib.utils.logging import LOG, measure_exec_time_decorator
 
 
+TCP_KEEP_IDLE = 45
+TCP_KEEPALIVE_INTERVAL = 10
+TCP_KEEP_CNT = 6
+
+
+class KeepAliveHTTPAdapter(HTTPAdapter):
+    """Transport adapter that allows us to use TCP Keep-Alive."""
+    def init_poolmanager(self, connections, maxsize, block=DEFAULT_POOLBLOCK, **pool_kwargs):
+        self.poolmanager = KeepAlivePoolManager(num_pools=connections, maxsize=maxsize,
+                                                block=block, strict=True, **pool_kwargs)
+
+
+class KeepAlivePoolManager(PoolManager):
+    """
+    This Pool Manager has only had the pool_classes_by_scheme variable changed.
+    This now points at the TCPKeepAlive connection pools rather than the default connection pools.
+    """
+    def __init__(self, num_pools=10, headers=None, **connection_pool_kw):
+        super().__init__(num_pools=num_pools, headers=headers, **connection_pool_kw)
+        self.pool_classes_by_scheme = {
+            "http": TCPKeepAliveHTTPConnectionPool,
+            "https": TCPKeepAliveHTTPSConnectionPool
+        }
+
+
+class TCPKeepAliveHTTPConnectionPool(HTTPConnectionPool):
+    """This class overrides the _validate_conn method in the HTTPConnectionPool class. This is the entry point to use
+    for modifying the socket as it is called after the socket is created and before the request is made."""
+    def _validate_conn(self, conn):
+        """Called right before a request is made, after the socket is created."""
+        super()._validate_conn(conn)
+        _tcp_keepalive_validation(conn)
+
+
+class TCPKeepAliveHTTPSConnectionPool(HTTPSConnectionPool):
+    """This class overrides the _validate_conn method in the HTTPSConnectionPool class. This is the entry point to use
+    for modifying the socket as it is called after the socket is created and before the request is made."""
+    def _validate_conn(self, conn):
+        """Called right before a request is made, after the socket is created."""
+        super()._validate_conn(conn)
+        _tcp_keepalive_validation(conn)
+
+
+# pylint: disable=no-member
+def _tcp_keepalive_validation(conn):
+    """Set up TCP Keep Alive probes"""
+    if sys.platform == 'win32':
+        # TCP Keep Alive Probes for Windows
+        conn.sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, TCP_KEEP_IDLE * 1000, TCP_KEEPALIVE_INTERVAL * 1000))
+    elif sys.platform == 'linux':
+        # TCP Keep Alive Probes for Linux/Android
+        # conn.sock could be of WrappedSocket type that not have socket methods (tested on RPI+LibreELEC)
+        _setsockopt = getattr(conn.sock, 'setsockopt', conn.sock.socket.setsockopt)
+        _setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        if hasattr(socket, 'TCP_KEEPIDLE'):
+            _setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, TCP_KEEP_IDLE)
+        if hasattr(socket, 'TCP_KEEPINTVL'):
+            _setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, TCP_KEEPALIVE_INTERVAL)
+        if hasattr(socket, 'TCP_KEEPCNT'):
+            _setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, TCP_KEEP_CNT)
+    elif sys.platform == 'darwin':
+        # TCP Keep Alive Probes for MacOS
+        # NOTE: The socket constants from MacOS netinet/tcp.h are not exported by python's socket module
+        conn.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        # The MacOS TCP_KEEPALIVE(0x10) constant should be the same thing of the linux TCP_KEEPIDLE constant
+        conn.sock.setsockopt(socket.IPPROTO_TCP, getattr(socket, 'TCP_KEEPIDLE', 0x10), TCP_KEEP_IDLE * 1000)
+        conn.sock.setsockopt(socket.IPPROTO_TCP, getattr(socket, 'TCP_KEEPINTVL', 0x101), TCP_KEEPALIVE_INTERVAL * 1000)
+        conn.sock.setsockopt(socket.IPPROTO_TCP, getattr(socket, 'TCP_KEEPCNT', 0x102), TCP_KEEP_CNT)
+
+
 class MSLRequests(MSLRequestBuilder):
     """Provides methods to make MSL requests"""
 
@@ -32,6 +106,8 @@ class MSLRequests(MSLRequestBuilder):
         super().__init__()
         from requests import session
         self.session = session()
+        self.session.mount('http://', KeepAliveHTTPAdapter())
+        self.session.mount('https://', KeepAliveHTTPAdapter())
         self.session.headers.update({
             'User-Agent': common.get_user_agent(),
             'Content-Type': 'text/plain',
@@ -178,8 +254,7 @@ class MSLRequests(MSLRequestBuilder):
                 LOG.debug('Request took {}s', time.perf_counter() - start)
                 LOG.debug('Request returned response with status {}', response.status_code)
                 break
-            except (req_exceptions.ConnectionError, req_exceptions.ReadTimeout) as exc:
-                # Info on PR: https://github.com/CastagnaIT/plugin.video.netflix/pull/1046
+            except req_exceptions.ConnectionError as exc:
                 LOG.error('HTTP request error: {}', exc)
                 if retry == 3:
                     raise
--- resources/lib/services/nfsession/session/base.py
+++ resources/lib/services/nfsession/session/base.py
@@ -8,10 +8,86 @@
     SPDX-License-Identifier: MIT
     See LICENSES/MIT.md for more information.
 """
+import socket
+import sys
+
+from requests.adapters import HTTPAdapter, DEFAULT_POOLBLOCK
+from urllib3 import PoolManager, HTTPConnectionPool, HTTPSConnectionPool
+
 import resources.lib.common as common
 from resources.lib.database.db_utils import TABLE_SESSION
 from resources.lib.globals import G
 from resources.lib.utils.logging import LOG
+
+
+TCP_KEEP_IDLE = 45
+TCP_KEEPALIVE_INTERVAL = 10
+TCP_KEEP_CNT = 6
+
+
+class KeepAliveHTTPAdapter(HTTPAdapter):
+    """Transport adapter that allows us to use TCP Keep-Alive."""
+    def init_poolmanager(self, connections, maxsize, block=DEFAULT_POOLBLOCK, **pool_kwargs):
+        self.poolmanager = KeepAlivePoolManager(num_pools=connections, maxsize=maxsize,
+                                                block=block, strict=True, **pool_kwargs)
+
+
+class KeepAlivePoolManager(PoolManager):
+    """
+    This Pool Manager has only had the pool_classes_by_scheme variable changed.
+    This now points at the TCPKeepAlive connection pools rather than the default connection pools.
+    """
+    def __init__(self, num_pools=10, headers=None, **connection_pool_kw):
+        super().__init__(num_pools=num_pools, headers=headers, **connection_pool_kw)
+        self.pool_classes_by_scheme = {
+            "http": TCPKeepAliveHTTPConnectionPool,
+            "https": TCPKeepAliveHTTPSConnectionPool
+        }
+
+
+class TCPKeepAliveHTTPConnectionPool(HTTPConnectionPool):
+    """This class overrides the _validate_conn method in the HTTPConnectionPool class. This is the entry point to use
+    for modifying the socket as it is called after the socket is created and before the request is made."""
+    def _validate_conn(self, conn):
+        """Called right before a request is made, after the socket is created."""
+        super()._validate_conn(conn)
+        _tcp_keepalive_validation(conn)
+
+
+class TCPKeepAliveHTTPSConnectionPool(HTTPSConnectionPool):
+    """This class overrides the _validate_conn method in the HTTPSConnectionPool class. This is the entry point to use
+    for modifying the socket as it is called after the socket is created and before the request is made."""
+    def _validate_conn(self, conn):
+        """Called right before a request is made, after the socket is created."""
+        super()._validate_conn(conn)
+        _tcp_keepalive_validation(conn)
+
+
+# pylint: disable=no-member
+def _tcp_keepalive_validation(conn):
+    """Set up TCP Keep Alive probes"""
+    if sys.platform == 'win32':
+        # TCP Keep Alive Probes for Windows
+        conn.sock.ioctl(socket.SIO_KEEPALIVE_VALS, (1, TCP_KEEP_IDLE * 1000, TCP_KEEPALIVE_INTERVAL * 1000))
+    elif sys.platform == 'linux':
+        # TCP Keep Alive Probes for Linux/Android
+        # conn.sock could be of WrappedSocket type that not have socket methods (tested on RPI+LibreELEC)
+        _setsockopt = getattr(conn.sock, 'setsockopt', conn.sock.socket.setsockopt)
+        _setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        if hasattr(socket, 'TCP_KEEPIDLE'):
+            _setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPIDLE, TCP_KEEP_IDLE)
+        if hasattr(socket, 'TCP_KEEPINTVL'):
+            _setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPINTVL, TCP_KEEPALIVE_INTERVAL)
+        if hasattr(socket, 'TCP_KEEPCNT'):
+            _setsockopt(socket.IPPROTO_TCP, socket.TCP_KEEPCNT, TCP_KEEP_CNT)
+    elif sys.platform == 'darwin':
+        # TCP Keep Alive Probes for MacOS
+        # NOTE: The socket constants from MacOS netinet/tcp.h are not exported by python's socket module
+        conn.sock.setsockopt(socket.SOL_SOCKET, socket.SO_KEEPALIVE, 1)
+        # The MacOS TCP_KEEPALIVE(0x10) constant should be the same thing of the linux TCP_KEEPIDLE constant
+        conn.sock.setsockopt(socket.IPPROTO_TCP, getattr(socket, 'TCP_KEEPIDLE', 0x10), TCP_KEEP_IDLE * 1000)
+        conn.sock.setsockopt(socket.IPPROTO_TCP, getattr(socket, 'TCP_KEEPINTVL', 0x101), TCP_KEEPALIVE_INTERVAL * 1000)
+        conn.sock.setsockopt(socket.IPPROTO_TCP, getattr(socket, 'TCP_KEEPCNT', 0x102), TCP_KEEP_CNT)
 
 
 class SessionBase:
@@ -39,6 +115,8 @@ class SessionBase:
             pass
         from requests import session
         self.session = session()
+        self.session.mount('http://', KeepAliveHTTPAdapter())
+        self.session.mount('https://', KeepAliveHTTPAdapter())
         self.session.max_redirects = 10  # Too much redirects should means some problem
         self.session.headers.update({
             'User-Agent': common.get_user_agent(enable_android_mediaflag_fix=True),
